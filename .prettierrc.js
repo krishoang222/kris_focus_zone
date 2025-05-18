f82d024b7ddb0fd16ef742bbd12ad16cece69797
krishoang <kristinker.work@gmail.com>
2025-05-18 13:01:12 +0700

    Enhaced: render timer with duration format mm:ss

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,5 +4,6 @@ module.exports = {
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
+  // Plugin: prettier-plugin-tailwindcss will help re-order position of class names to fix priority
   plugins: ['prettier-plugin-tailwindcss'],
 };
